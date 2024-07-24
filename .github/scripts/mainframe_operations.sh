100d589ab406cfc843c05ab3d7e524c9ee96b875
Ali <65877768+alim395@users.noreply.github.com>
2024-07-24 13:50:01 -0400

    Update mainframe_operations.sh
    Added operations for EMPPAY and DEPTPAY.

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -20,11 +20,26 @@ chmod +x linux_gnucobol_run_tests
 cd ..
 pwd
 
-# Run COBOL check
+# Run COBOL check on NUMBERS
 ./cobolcheck -p NUMBERS
 
-# Copy files to datasets
+# Copy NUMBERS files to datasets
 cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL(NUMBERS)'"
 cp NUMBERS.JCL "//'${ZOWE_USERNAME}.JCL(NUMBERS)'"
+
+# Run COBOL check on EMPPAY
+./cobolcheck -p EMPPAY
+
+# Copy EMPPAY files to datasets
+cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL(EMPPAY)'"
+cp EMPPAY.JCL "//'${ZOWE_USERNAME}.JCL(EMPPAY)'"
+
+# Run COBOL check on DEPTPAY
+./cobolcheck -p DEPTPAY
+
+# Copy DEPTPAY files to datasets
+cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL(DEPTPAY)'"
+cp DEPTPAY.JCL "//'${ZOWE_USERNAME}.JCL(DEPTPAY)'"
+
 
 echo "Mainframe operations completed"
